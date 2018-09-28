41e2a2dc44fcea03a90274d5e477e333ff4d4718
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-28 21:53:40 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string_sentence)
   array_sentence = string_sentence.split(" ")
-  array_sentence.collect do |word|
+  reversed_array = array_sentence.collect do |word|
     word = word.reverse 
   end 
-  array_sentence.join(" ")
+
 end 
